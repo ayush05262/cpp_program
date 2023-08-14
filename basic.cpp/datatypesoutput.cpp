160f784402369d08d93c4e14edaa1c87e528399d
ayush05262 <ayush05262@gmail.com>
2023-08-14 18:57:18 +0530

    .

---
+++ basic.cpp/datatypesoutput.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a= 5, b= 6;
+    float pi = 1.34;
+    char m = 'k';
+    cout<<"the value of a = "<<a<< "\n the value of b = "<<b;
+    cout<< "\n the of pi is " <<pi;
+    cout<<"\n the value of m= " <<m;
+    return 0;
+
+}
---
+++ basic.cpp/number_add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int bio, cio ;
+    cout<<"enter bio";
+    cin>> bio;
+    cout<<"inter cio";
+    cin>> cio;
+    cout<< "the sum of bio and cio is : "<<bio + cio ;
+    return 0;
+
+}
